1f988297ad11b9ca62859f9e36a6781b90311443
lfc <lufer.lfcc@gmail.com>
2019-07-27 11:25:38 -0700

    Summary ======= - add drivers Chrome, headless, Firefox, docker

--- src/main/java/com/webaut/project/pages/AbstractPage.java
+++ src/main/java/com/webaut/project/pages/AbstractPage.java
@@ -7,15 +7,16 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AbstractPage {
+
     protected WebDriver driver;
     protected WebDriverWait wait;
     protected WebDriverAction action;
 
     public AbstractPage() {
         driver = DriverManager.getInstance().getDriver();
-        wait = DriverManager.getInstance().getWait();
+        wait = new WebDriverWait(driver, 30);
         action = new WebDriverAction(driver, wait);
 
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
     }
 }
