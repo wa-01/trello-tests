f7932a598d49f95a91bd9943619497f39db8341c
JALASOFT\Gilmar Pozzo <gilmar.pozzo@jalasoft.com>
2019-07-29 20:58:01 -0400

    Changing Class Name from 'Dashboards' to 'Home' and Removing 'HomePage' one

--- src/main/java/com/webaut/project/pages/Home.java
+++ src/main/java/com/webaut/project/pages/Home.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class Dashboards extends AbstractPage {
+public class Home extends AbstractPage {
     //TEAMS Section of Side bar
     //-------------------------------------------------------------------------
     @FindBy(css = "button[data-test-id='home-navigation-create-team-button']")
