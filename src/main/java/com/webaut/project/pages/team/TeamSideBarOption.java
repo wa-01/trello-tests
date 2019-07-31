50304df38c0e08e515d451e37769f7b996e6bd8f
JALASOFT\Gilmar Pozzo <gilmar.pozzo@jalasoft.com>
2019-07-31 17:37:55 -0400

    Adding wait.until(ExpectedConditions.visibilityOf())

--- src/main/java/com/webaut/project/pages/team/TeamSideBarOption.java
+++ src/main/java/com/webaut/project/pages/team/TeamSideBarOption.java
@@ -1,23 +1,31 @@
 package com.webaut.project.pages.team;
 
 import com.webaut.project.pages.AbstractPage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class TeamSideBarOption extends AbstractPage {
 
     @FindBy(xpath = "//span[text() = 'Settings']")
     private WebElement teamSettings;
 
-    public void clickSettings(){
-        action.click(teamSettings);
-    }
+    @FindBy(xpath = "//span[text() = 'Cards']")
+    private WebElement teamCards;
+
+    @FindBy(css = ".u-inline")
+    private WebElement teamNameAsTitle;
 
     @FindBy(css = ".quiet-button")
     private WebElement deleteTeamLink;
 
+    public void clickSettings(){
+        wait.until(ExpectedConditions.visibilityOf(teamCards));
+        action.click(teamSettings);
+    }
+
     public void clickDeleteLink(){
+        wait.until(ExpectedConditions.visibilityOf(teamNameAsTitle));
         action.click(deleteTeamLink);
     }
 }
