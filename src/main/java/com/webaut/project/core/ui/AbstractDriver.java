ce300026dad7cdb4fb1cf6e6d099aea2c6d69147
Jose colina <jose.colina@jalasoft.com>
2019-08-04 11:28:57 -0400

    test travis with copy dev branch

--- src/main/java/com/webaut/project/core/ui/AbstractDriver.java
+++ src/main/java/com/webaut/project/core/ui/AbstractDriver.java
@@ -3,6 +3,6 @@ package com.webaut.project.core.ui;
 import org.openqa.selenium.WebDriver;
 
 abstract class AbstractDriver {
-
+    //comment to test travis with dev branch
     abstract WebDriver initDriver();
 }
