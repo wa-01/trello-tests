f89b12e7911f4b8dcebbae9fa576eba3741534e8
Brayan Rosas <brayan_rosas@hotmail.com>
2019-08-05 21:44:13 -0400

    Optimizing and fixing problems with locators.

--- src/main/java/com/webaut/project/pages/Header.java
+++ src/main/java/com/webaut/project/pages/Header.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.FindBy;
 public class Header extends AbstractPage {
 
     private static final String MEMBER_BUTTON = "//button[contains(@title,\"%s\")]";
+    private static final String XPATH_BOARD_LINK_IN_TEAM ="//span[text()='%s']/../../following-sibling::div//a[contains(@href,'%s')]";
 
     @FindBy(css = "button[data-steps-id = 'header-member-menu-button']")
     private WebElement memberButton;
@@ -22,6 +23,12 @@ public class Header extends AbstractPage {
 
     @FindBy(css = "span[name= 'add']")
     private WebElement addButton;
+
+    @FindBy(css = "a[data-test-id='home-link']")
+    private WebElement homeLink;
+
+    @FindBy(css = "button[data-test-id=\"header-boards-menu-button\"]")
+    private WebElement boardMenuButton;
 
     public Boolean userNameIsOnButton(String userName) {
         return action.isElementVisible(By.xpath(String.format(MEMBER_BUTTON, userName)));
@@ -49,6 +56,24 @@ public class Header extends AbstractPage {
     }
 
     public void goToHomeFromTeam() {
+
         driver.get("https://trello.com/");
+        action.click(homeLink);
     }
+
+    public BoardDetails goToBoardDetailsFromBoardMenu(String boardTeam,String boardID){
+        if(boardTeam.equals("No team")) boardTeam = "Personal Boards";
+        String specificBoardLink = String.format(XPATH_BOARD_LINK_IN_TEAM,boardTeam,boardID);
+        action.click(boardMenuButton);
+        action.click(By.xpath(specificBoardLink));
+        return new BoardDetails();
+    }
+
+    public String isBoardInRecentBoardsList(String boardTeam,String boardID) {
+        if(boardTeam.equals("No team")) boardTeam = "Personal Boards";
+        String boardLink = String.format(XPATH_BOARD_LINK_IN_TEAM, boardTeam,boardID);
+        System.out.println(driver.findElement(By.xpath(boardLink)).getAttribute("title"));
+        return driver.findElement(By.xpath(boardLink)).getAttribute("title");
+    }
+
 }
